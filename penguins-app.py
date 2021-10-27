81f412e242b6913f7ebefa62ce9437f6b427fa1b
Dhinahar12 <87924540+Dhinahar12@users.noreply.github.com>
2021-10-27 19:32:21 +0530

    Bug Fixes

--- penguins-app.py
+++ penguins-app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
-
+from PIL import Image
 
 image= Image.open('penguin.png')
 st.image(image, use_column_width=True) 
